9a462f978e6b72cb37d381f4261d08d3aede39b0
Paul Murray <pmurray@bigpond.com>
2015-12-03 23:01:42 +1100

    fix

--- forum-topic-363073.ino
+++ forum-topic-363073.ino
@@ -1,11 +1,24 @@
 
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 1
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
+
 // we want to randomly select from these colours
 
 const int COLOURS[][3] = {
-{100, 200, 255}, //Blue
-{255, 250, 170}, //Yellow
-{255, 170, 255}, //Pink
-{255, 255, 255} //White
+  {100, 200, 255}, //Blue
+  {255, 250, 170}, //Yellow
+  {255, 170, 255}, //Pink
+  {255, 255, 255} //White
 };
 
 const int NCOLOURS = sizeof(COLOURS) / sizeof(*COLOURS);
@@ -32,70 +45,75 @@ struct PixelState {
 const int NPIXELS = 4;
 struct PixelState pixel[NPIXELS];
 
-// I want the pixels to sparkle, so I will use a triangle wave, 
+// I want the pixels to sparkle, so I will use a triangle wave,
 // which is to say that they will come on at full brightness and them fade
 
-const unsigned long FADE_TIME_MS = 3000; // three seconds
+const int FADE_TIME_MS = 3000; 
 
-// the pixels should come on at a random time, To simulate this, 
-// the pixel will be off for a random amount of time. 
+// the pixels should come on at a random time, To simulate this,
+// the pixel will be off for a random amount of time.
 
-const int MIN_OFF_TIME_MS = 100;
-const int MAX_OFF_TIME_MS = 250;
+const int MIN_OFF_TIME_MS = 500;
+const int MAX_OFF_TIME_MS = 1500;
 
 void setup() {
-  for(int i = 0; i<NPIXELS;i++) {
+  for (int i = 0; i < NPIXELS; i++) {
     pixel[i].on = false;
     pixel[i].timeMarkMs = millis();
     pixel[i].offTimeMs = 0;
   }
 }
 
-// this is a dummy function. I put it here just to check that the code compiles.
-// Replace calls to this function with your
-// strip.setPixelColor function
-void setPixelColor(int strip, int r, int g, int b) {
-}
-
 void loop() {
-  for(int i = 0; i< NPIXELS; i++) {
-    if(pixel[i].on) {
+  for (int i = 0; i < NPIXELS; i++) {
+    if (pixel[i].on) {
       // pixel is on
-      
-      if(millis() - pixel[i].timeMarkMs >= FADE_TIME_MS) {
+
+      if (millis() - pixel[i].timeMarkMs >= FADE_TIME_MS) {
         // time to turn the pixel off
-        setPixelColor(i,0,0,0);
+        strip.setPixelColor(i, 0, 0, 0);
         pixel[i].timeMarkMs = millis();
         pixel[i].offTimeMs = random(MIN_OFF_TIME_MS, MAX_OFF_TIME_MS);
+        Serial.print("off time");
+        Serial.print( pixel[i].offTimeMs);
+        Serial.println();
         pixel[i].mostRecentBrightness = 0;
         pixel[i].on = false;
       }
       else {
         // calculate the new brightness as flaoting point
         // this is the bit that you change if you want the fade pattern to change
-        float brightness = 1 - (millis() - pixel[i].timeMarkMs)/((float)FADE_TIME_MS);
-        
+        float brightness = 1 - (millis() - pixel[i].timeMarkMs) / ((float)FADE_TIME_MS);
+        brightness = brightness * brightness; //gamma correction
+
         // ok. do we actually need to change the pixel colour?
         int mostRecentBrightness = 255 * brightness;
-        if(mostRecentBrightness != pixel[i].mostRecentBrightness) {
+        if (mostRecentBrightness != pixel[i].mostRecentBrightness) {
           // yes we do
           pixel[i].mostRecentBrightness = mostRecentBrightness;
-          setPixelColor(i,
-             (int)(COLOURS[pixel[i].selectedColour][0] * brightness),
-             (int)(COLOURS[pixel[i].selectedColour][1] * brightness),
-             (int)(COLOURS[pixel[i].selectedColour][2] * brightness)
-          );
+          strip.setPixelColor(i,
+                        (int)(COLOURS[pixel[i].selectedColour][0] * brightness),
+                        (int)(COLOURS[pixel[i].selectedColour][1] * brightness),
+                        (int)(COLOURS[pixel[i].selectedColour][2] * brightness)
+                       );
         }
       }
     }
     else {
       // pixel is off. do we need to turn it on?
-      
-      if(millis()-pixel[i].timeMarkMs > pixel[i].offTimeMs) {
+
+      if (millis() - pixel[i].timeMarkMs > pixel[i].offTimeMs) {
         pixel[i].on = true;
         pixel[i].mostRecentBrightness = 0; // this will force an update next loop
         pixel[i].selectedColour = random(NCOLOURS);
+        pixel[i].timeMarkMs = millis();
+        strip.setPixelColor(i,
+                      COLOURS[pixel[i].selectedColour][0],
+                      COLOURS[pixel[i].selectedColour][1],
+                      COLOURS[pixel[i].selectedColour][2]
+                     );
       }
     }
   }
 }
+
